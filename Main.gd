6d048397b90f5f3f45c7340c96d67b1ffc07f73f
ModeratePrawn <shane.lawson91@gmail.com>
2022-06-22 17:50:30 -0400

    Add files via upload
    V0.1.5
    
    Updates:
    
    Added Weather Alerts

--- Main.gd
+++ Main.gd
@@ -16,7 +16,13 @@ var CatForecastURL
 var WeeklyForecastResult
 var HourlyForecastResult
 var DetailedForecastNumebr
-var CityState = str(city,state)
+var CityState
+var ZoneURL
+var ZoneID
+var AlertBase = "https://api.weather.gov/alerts/active/zone/"
+var ActiveAlertURL
+var Alerts
+var AlertCount
 
 func _ready():
 	print("Program Starting")
@@ -36,6 +42,8 @@ func _on_Location_request_completed(result, response_code, headers, body):
 	zipCode = json.result.zip
 	lat = json.result.lat
 	long = json.result.lon
+	#lat = 33.4025 #For Testing. 
+	#long = -84.522 #For Testing
 	fullURL = str(baseURL, lat, ",", long)
 	CityState = str(city," ",state)
 	$GUI/Forecasts/Window/Header/Location.text = String(CityState)
@@ -57,21 +65,28 @@ func _on_Forecast_request_completed(result, response_code, headers, body):
 	ForecastURL = GPjson.result.properties.forecast
 	HForecastURL = GPjson.result.properties.forecastHourly
 	CatForecastURL = str(ForecastURL,ImpUnits)
+	ZoneURL = GPjson.result.properties.county
 	print("Forecast URL:")
 	print(ForecastURL)
 	print("Hourly URL:")
 	print(HForecastURL)
 	print("Full Weekly URL:")
 	print(CatForecastURL)
+	print("Zone URL:")
+	print(ZoneURL)
 	print("Getting Weekly Forecast")
 	getHourlyForecast()
 	getWeeklyForecast()
+	getZoneID()
 
 func getHourlyForecast():
 	$NetworkController/HoulyForecast.request(HForecastURL)
 
 func getWeeklyForecast():
 	$NetworkController/WeeklyForecast.request(CatForecastURL)
+
+func getZoneID():
+	$NetworkController/ZoneIDRequest.request(ZoneURL)
 
 func _on_HoulyForecast_request_completed(result, response_code, headers, body):
 	var HourlyForecastJSON = JSON.parse(body.get_string_from_utf8())
@@ -82,6 +97,85 @@ func _on_WeeklyForecast_request_completed(result, response_code, headers, body):
 	var WeeklyForecastJSON = JSON.parse(body.get_string_from_utf8())
 	WeeklyForecastResult = WeeklyForecastJSON.result.properties.periods
 	PrintWeeklyResults()
+
+func _on_ZoneIDRequest_request_completed(result, response_code, headers, body):
+	var ZoneIDRequestResult = JSON.parse(body.get_string_from_utf8())
+	ZoneID = ZoneIDRequestResult.result.properties.id
+	ActiveAlertURL = str(AlertBase,ZoneID)
+	print("ZoneID:")
+	print(ZoneID)
+	print("Active Alerts URL:")
+	print(ActiveAlertURL)
+	getAlerts()
+
+func getAlerts():
+	$NetworkController/AlertRequest.request(ActiveAlertURL)
+
+func _on_AlertRequest_request_completed(result, response_code, headers, body):
+	var AlertJSON = JSON.parse(body.get_string_from_utf8())
+	Alerts = AlertJSON.result.features
+	AlertHandler()
+
+func AlertHandler():
+	print("Alerts:")
+	if Alerts == []:
+		print("No Alerts")
+		$GUI/Forecasts/Window/NowCast/Alert.visible = false
+	else: 
+		$GUI/Forecasts/Window/NowCast/Alert.visible = true
+		print("ALERT!")
+		AlertCount = Alerts.size()
+		#AlertCount = 3 #Testing.
+		print("Alert Count:")
+		print(AlertCount)
+		alertProcessing()
+
+func alertProcessing():
+	var i = AlertCount
+	var Counter = 0
+	var AlertNumber = 1
+	var scene = ResourceLoader.load("res://AlertInfo.tscn")
+	var path = get_tree().get_root().get_node("Main/GUI/AlertDisplay/Body/AlertScroll/Target")
+	print(path)
+	print(scene)
+	print(i)
+		
+	for number in range(i):
+		#Make Instance
+		print("Loop:", Counter)
+		var NewInstance = scene.instance()
+		NewInstance.name = "AlertInfo" + String(Counter)
+		path.add_child(NewInstance)
+		print("New Instance Name:")
+		print(NewInstance.name)
+		NewInstance.get_node("Intro/AlertNum").text = String(AlertNumber)
+		NewInstance.get_node("Headline/HLabel").text = String(Alerts[Counter].properties.headline)
+		NewInstance.get_node("AlertInfo1/A1Status").text = String(Alerts[Counter].properties.severity)
+		NewInstance.get_node("AlertInfo2/A2Status").text = String(Alerts[Counter].properties.certainty)
+		NewInstance.get_node("AlertInfo3/A3Status").text = String(Alerts[Counter].properties.urgency)
+		NewInstance.get_node("AlertInfo4/A4Status").text = String(Alerts[Counter].properties.event)
+		NewInstance.get_node("AlertInfo5/A5Status").text = String(Alerts[Counter].properties.response)
+		NewInstance.get_node("Desc").text = String(Alerts[Counter].properties.description)
+		print("Instructions:")
+		print(Alerts[Counter].properties.instruction)
+		
+		if Alerts[Counter].properties.instruction == null:
+			print("NULL!!!!")
+			NewInstance.get_node("Ins").text = "No Further Instructions at this Time"
+		else:
+			print("Instructions Present")
+			NewInstance.get_node("Ins").text = String(Alerts[Counter].properties.instruction)
+		
+		Counter = Counter + 1
+		AlertNumber = AlertNumber + 1
+
+func _on_Alert_pressed():
+	$GUI/AlertDisplay.visible = true
+	$GUI/Forecasts.visible = false
+
+func _on_ForecastReturn_pressed():
+	$GUI/AlertDisplay.visible = false
+	$GUI/Forecasts.visible = true
 
 func PrintHourlyResults():
 	#NowCast
@@ -99,6 +193,7 @@ func PrintHourlyResults():
 
 func PrintWeeklyResults():
 	$GUI/Forecasts/Window/WeeklyScroll.get_v_scrollbar().modulate = Color(0, 0, 0, 0) #Hide Scroll Bar
+	$GUI/AlertDisplay/Body/AlertScroll.get_v_scrollbar().modulate = Color(0, 0, 0, 0) #Hide Scroll Bar
 	$GUI/Forecasts/Window/WeeklyScroll/WeekCast/W0/Date.text = String(WeeklyForecastResult[0].name)
 	$GUI/Forecasts/Window/WeeklyScroll/WeekCast/W0/W0Temp/Temp.text = String(WeeklyForecastResult[0].temperature)
 	$GUI/Forecasts/Window/WeeklyScroll/WeekCast/W0/W0Temp/Unit.text = String(WeeklyForecastResult[0].temperatureUnit)
